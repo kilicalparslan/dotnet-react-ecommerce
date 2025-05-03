e12a0f2bb80c20f88c1f44577b22fcc108386676
kilicalparslan <kilicalparslan1998@gmail.com>
2025-05-03 21:44:54 +0300

    rtk store added

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router/Routes";
+import { Provider } from "react-redux";
 import { CartContextProvider } from "./context/CartContext";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CartContextProvider>
-      <RouterProvider router={router} />
-    </CartContextProvider>
+    <Provider store={store}>
+      <CartContextProvider>
+        <RouterProvider router={router} />
+      </CartContextProvider>
+    </Provider>
   </React.StrictMode>
 );
