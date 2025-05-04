4e3b8bb8959d1e4550a40c9bb4c73aa1b5b95821
kilicalparslan <kilicalparslan1998@gmail.com>
2025-05-04 23:31:36 +0300

    asyncthunk functions added

--- Client/src/pages/cart/cartSlice.ts
+++ Client/src/pages/cart/cartSlice.ts
@@ -1,13 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Cart } from "../../model/ICart";
+import requests from "../../api/requests";
 
 interface CartState {
   cart: Cart | null;
+  status: string;
 }
 
 const initialState: CartState = {
   cart: null,
+  status: "idle",
 };
+
+export const addItemToCart = createAsyncThunk<
+  Cart,
+  { productId: number; quantity?: number }
+>("cart/addItemToCart", async ({ productId, quantity = 1 }) => {
+  try {
+    return await requests.cart.addItem(productId, quantity);
+  } catch (error) {
+    console.error("Error adding item to cart:", error);
+  }
+});
+
+export const deleteItemFromCart = createAsyncThunk<
+  Cart,
+  { productId: number; quantity?: number }
+>("cart/deleteItemFromCart", async ({ productId, quantity = 1 }) => {
+  try {
+    return await requests.cart.deleteItem(productId, quantity);
+  } catch (error) {
+    console.error("Error adding item to cart:", error);
+  }
+});
 
 export const cartSlice = createSlice({
   name: "cart",
@@ -19,6 +44,30 @@ export const cartSlice = createSlice({
     clearCart: (state) => {
       state.cart = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addItemToCart.pending, (state, action) => {
+      console.log("pending", action);
+      state.status = "loading";
+    });
+    builder.addCase(addItemToCart.fulfilled, (state, action) => {
+      state.cart = action.payload;
+      state.status = "idle";
+    });
+    builder.addCase(addItemToCart.rejected, (state) => {
+      state.status = "idle";
+    });
+    builder.addCase(deleteItemFromCart.pending, (state, action) => {
+      console.log("pending", action);
+      state.status = "loading";
+    });
+    builder.addCase(deleteItemFromCart.fulfilled, (state, action) => {
+      state.cart = action.payload;
+      state.status = "idle";
+    });
+    builder.addCase(deleteItemFromCart.rejected, (state) => {
+      state.status = "idle";
+    });
   },
 });
 
