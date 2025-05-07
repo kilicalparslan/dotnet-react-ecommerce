019576fd65a00e454dd34f869fea79c5fbd1ceac
kilicalparslan <kilicalparslan1998@gmail.com>
2025-05-07 21:32:54 +0300

    accountcontroller created

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+using API.Dto;
+using API.DTO;
+using API.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public AccountController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto model)
+    {
+        var user = await _userManager.FindByNameAsync(model.UserName);
+
+        if (user == null)
+        {
+            return BadRequest(new { message = "Invalid username or password" });
+        }
+
+        var result = await _userManager.CheckPasswordAsync(user, model.Password);
+
+        if (result)
+        {
+            return Ok(new { token = "token" });
+        }
+
+        return Unauthorized();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDto model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = new AppUser
+        {
+            Name = model.Name,
+            UserName = model.UserName,
+            Email = model.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            await _userManager.AddToRoleAsync(user, "Customer");
+            return StatusCode(201);
+        }
+
+        return BadRequest(result.Errors);
+    }
+}
