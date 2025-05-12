a30954b1f76203b9d835b7bcedf7ca5dba7779d3
Alp <kilicalparslan1998@gmail.com>
2025-05-12 10:36:01 +0300

    checkoutpage created w using stepper

--- Client/src/features/checkout/CheckoutPage.tsx
+++ Client/src/features/checkout/CheckoutPage.tsx
@@ -1,3 +1,98 @@
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+} from "@mui/material";
+import Info from "./Info";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import Review from "./Review";
+import { useState } from "react";
+import { ChevronLeftRounded, ChevronRightRounded } from "@mui/icons-material";
+
+const steps = ["Delivery Information", "Payment", "Order Summary"];
+
+function getStepContent(step: number) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <PaymentForm />;
+    case 2:
+      return <Review />;
+    default:
+      throw new Error("Unknown step");
+  }
+}
+
 export default function CheckoutPage() {
-  return <h1>Checkout</h1>;
+  const [activeStep, setActiveStep] = useState(0);
+  function handleNext() {
+    setActiveStep(activeStep + 1);
+  }
+  function handlePrevious() {
+    setActiveStep(activeStep - 1);
+  }
+  return (
+    <Paper>
+      <Grid container sx={{ p: 4 }} spacing={4}>
+        <Grid size={4}>
+          <Info />
+        </Grid>
+        <Grid size={8}>
+          <Box>
+            <Stepper activeStep={activeStep} sx={{ height: 40 }}>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Box>
+          <Box>
+            {activeStep === steps.length ? (
+              <h2>Order completed.</h2>
+            ) : (
+              <>
+                {getStepContent(activeStep)}
+                <Box>
+                  <Box
+                    sx={[
+                      {
+                        display: "flex",
+                      },
+                      activeStep !== 0
+                        ? { justifyContent: "space-between" }
+                        : { justifyContent: "flex-end" },
+                    ]}
+                  >
+                    {activeStep !== 0 && (
+                      <Button
+                        startIcon={<ChevronLeftRounded />}
+                        variant="contained"
+                        onClick={handlePrevious}
+                      >
+                        Back
+                      </Button>
+                    )}
+                    <Button
+                      startIcon={<ChevronRightRounded />}
+                      variant="contained"
+                      onClick={handleNext}
+                    >
+                      Next
+                    </Button>
+                  </Box>
+                </Box>
+              </>
+            )}
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
---
+++ Client/src/features/checkout/Info.tsx
@@ -0,0 +1,33 @@
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { useAppSelector } from "../../store/store";
+import { currencyUSD } from "../../utils/formatCurrency";
+
+export default function Info() {
+  const { cart } = useAppSelector((state) => state.cart);
+  const subTotal =
+    cart?.cartItems.reduce(
+      (sum, item) => sum + item.quantity * item.price,
+      0
+    ) ?? 0;
+  return (
+    <>
+      <Typography variant="subtitle2" sx={{ color: "text.secondary" }}>
+        Total
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        {currencyUSD.format(subTotal)}
+      </Typography>
+      <List>
+        {cart?.cartItems.map((item) => (
+          <ListItem key={item.productId} sx={{py:1, px:0}}>
+            <ListItemAvatar>
+              <Avatar variant="square" src={`http://localhost:5057/images/${item.imageUrl}`}></Avatar>
+            </ListItemAvatar>
+            <ListItemText sx={{mr:2}} primary={item.name} secondary={`x${item.quantity}`}></ListItemText>
+            <Typography variant="body1">{currencyUSD.format(item.price)}</Typography>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
